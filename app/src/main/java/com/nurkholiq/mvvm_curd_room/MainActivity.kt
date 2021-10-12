1cf8ab69f39f4b4ab460e3db6589a38aad2c4171
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-12 10:51:26 +0700

    Modified MainActivity.kt dengan menggunakan dataBinding dan menggunakan data langsung dengan menyediakan lifecycleOwner

--- app/src/main/java/com/nurkholiq/mvvm_curd_room/MainActivity.kt
+++ app/src/main/java/com/nurkholiq/mvvm_curd_room/MainActivity.kt
@@ -2,10 +2,27 @@ package com.nurkholiq.mvvm_curd_room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.nurkholiq.mvvm_curd_room.databinding.ActivityMainBinding
+import com.nurkholiq.mvvm_curd_room.db.SubscriberDatabase
+import com.nurkholiq.mvvm_curd_room.db.SubscriberRepository
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val dao = SubscriberDatabase.getInstance(application).subscriberDAO
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+
+        subscriberViewModel = ViewModelProvider(this, factory).get(SubscriberViewModel::class.java)
+        binding.myViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
     }
 }
